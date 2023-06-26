e771367594dce2f1667b65e904ff1023963c6b05
Daniel Eduardo Macias Estrada <danma1708@gmail.com>
2023-06-26 00:00:27 -0600

    Update LoginController.php

--- devstagram/app/Http/Controllers/LoginController.php
+++ devstagram/app/Http/Controllers/LoginController.php
@@ -5,26 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
-{
+{    
+    // Método que muestra la vista del login
     public function index()
     {
         return view('auth.login');
     }
 
+    // Método para iniciar sesión
     public function store(Request $request)
     {
+        
         $this->validate($request, [
             'email' => 'required|email',
             'password' => 'required'
         ]);
 
-        // Condicion para saber si el user se pudo autenticar
+        // Se valida si las credenciales son correctas
         if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
             // back() para volver a la pagina anterior, en este caso, con un mensaje
             return back()->with('mensaje', 'Credenciales Incorrectas');
         }
 
-        // Redirecciona
+        // Se redirecciona a la página principal del usuario en caso de ser correcto
         return redirect()->route('posts.index', auth()->user()->username);
     }
 }
